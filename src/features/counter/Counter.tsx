0197ac69e77119a5d908ab53c0122c800092059c
witcher-vae <dhuuloc8818@gmail.com>
2021-08-18 20:08:01 +0700

    initial project

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
 
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+import { decrement, increment, incrementByAmount, incrementAsync, incrementIfOdd, selectCount } from './counterSlice';
 
 export function Counter() {
   const count = useAppSelector(selectCount);
@@ -20,48 +12,20 @@ export function Counter() {
 
   return (
     <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
+      <div>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
           -
         </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
+        <span>{count}</span>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
           +
         </button>
       </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
+      <div>
+        <input aria-label="Set increment amount" value={incrementAmount} onChange={e => setIncrementAmount(e.target.value)} />
+        <button onClick={() => dispatch(incrementByAmount(incrementValue))}>Add Amount</button>
+        <button onClick={() => dispatch(incrementAsync(incrementValue))}>Add Async</button>
+        <button onClick={() => dispatch(incrementIfOdd(incrementValue))}>Add If Odd</button>
       </div>
     </div>
   );
